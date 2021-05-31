3be5768af4f5191605c084e94677f6d55abc04b1
Pranav Patel <Pranav.Patel@trigyn.com>
2021-05-31 16:35:30 +0530

    Add project files.

---
+++ DaprDemoApp/Data/FeedbackService.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace DaprDemoApp.Data
+{
+    /// <summary>
+    /// Service to handle user feedback
+    /// </summary>
+    public class FeedbackService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="httpClientFactory"></param>
+        /// <param name="logger"></param>
+        public FeedbackService(IHttpClientFactory httpClientFactory, ILogger<FeedbackService> logger)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// SubmitFeedback - Accepts user feedback and publishes User
+        //  message to DAPR Sidecar pub-sub endpoint
+        /// </summary>
+        /// <param name="userData"></param>
+        /// <returns></returns>
+        public async Task<bool> SubmitFeedback(UserFeedback userData)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            HttpContent httpContent = new StringContent(JsonSerializer.Serialize(userData));
+            _logger.LogInformation(JsonSerializer.Serialize(userData));
+
+            //DAPR PUB-SUB URL 
+            //http://localhost:3500/v1.0/publish/<pub-sub-component-name>/<topic-name>
+            try
+            {
+                 string daprPort= Environment.GetEnvironmentVariable("DAPR_HTTP_PORT");
+                _logger.LogInformation("dapper port  " + daprPort);
+                var responsePingService = await client.GetAsync("http://localhost:" + daprPort + "/v1.0/invoke/feedbackapi/method/Feedback/Ping");
+                //              ;
+                 _logger.LogInformation(responsePingService.Content.ToString());
+                //if (responsePingService.IsSuccessStatusCode)
+                //{
+                //    _logger.LogInformation(responsePingService.Content.ToString());
+                //    return true;
+                //}
+
+                var response = await client.PostAsync("http://localhost:" + daprPort + "/v1.0/publish/messagebus/ReceiveFeedback",
+                               httpContent);
+
+
+                //var response = await client.PostAsync("http://localhost:" + daprPort + "/v1.0/invoke/feedbackapi/method/Feedback/ReceiveFeedback",
+                //               new StringContent(JsonSerializer.Serialize(userData.FirstName)));
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation($"Posted message successfuly - {userData.FirstName}");
+                    return true;
+                }
+            }
+            catch (System.Exception e)
+            {
+
+                throw;
+            }
+            
+           
+
+            _logger.LogError("Something went wrong");
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# DaprDemoApp
+ Dapr Demp Application - Server Side Blazor Application - Publishes to local queue component defined by DAPR
+
+# DAPR
+
+Dapr is  Distributed Application runtime - An open source, event driven , portable runtime that helps to build distributed system on cloud and on edge systems.
+
+Visit DAPR Official Documentation at - [DAPR](https://dapr.io/)
+
+# About this Repository
+
+*Dapr Demo App*
+
+ Dapr Demp Application - Server Side Blazor Application - Publishes to local queue component defined by DAPR
+
+This repository contains the front end code and publishes to local component *pubsub.redis* defined as a component. 
+
+Backend API and Component Definition Available at - [Dapr Demo API](https://github.com/amreshkris/daprDemoAPI)
+
+# Architecture Diagram
+
+<img src="./DaprDemoApp/Images/Demo_ArchitectureDiagram.png">
+
